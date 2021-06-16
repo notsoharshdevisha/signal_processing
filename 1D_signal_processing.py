34f135de0d2badb652142ab585a7b622e3b9a68a
notsoharshdevisha <85506475+notsoharshdevisha@users.noreply.github.com>
2021-06-16 21:31:07 +0530

    Add files via upload

--- 1D_signal_processing.py
+++ 1D_signal_processing.py
@@ -3,6 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.neighbors import KNeighborsRegressor
 from Convolution import Convolution
+#import seaborn as sns
+#sns.set()
 
 #generating signal
 t = np.arange(0,10, 0.01)
@@ -11,17 +13,15 @@ err = np.random.rand(len(t))
 signal = orig_signal + err * 1.5
 fig, ax = plt.subplots(nrows = 2)
 ax[0].plot(t, orig_signal, linewidth=1)
-ax[0].set_title('Original Signal')
-ax[0].set_xlabel("Time Index")
-ax[0].set_ylabel('Amplitude')
-ax[0].grid()
+ax[0].set_title('Original Signal', fontsize=10)
+ax[0].set_xlabel("Time Index", fontsize=5)
+ax[0].set_ylabel('Amplitude', fontsize=5)
 ax[0].set_xlim(0, 10)
 ax[1].plot(t, signal)
-ax[1].set_title('Signal with induced error')
-ax[1].set_xlabel("Time Index")
-ax[1].set_ylabel('Amplitude')
-ax[1].grid()
-ax[1].set_xlim(1, 10)
+ax[1].set_title('Signal with induced error', fontsize=10)
+ax[1].set_xlabel("Time Index", fontsize=5)
+ax[1].set_ylabel('Amplitude', fontsize=5)
+ax[1].set_xlim(0, 10)
 fig.tight_layout()
 plt.show()
 
@@ -40,55 +40,35 @@ ax.plot(t, blackman, label = 'balckman')
 ax.plot(t, flat, label = 'flat')
 ax.plot(t, bartlett, label = 'bartlett')
 ax.legend(loc = 1)
-ax.grid()
-ax.set_title('Windows')
+ax.set_title('Windows', fontsize=10)
 plt.show()
 
 #perfroming convolution
+
+#with convolution module
 window_len = 15
 conv_obj = Convolution()
 out_signal = conv_obj.convolute('flat', window_len, signal)
-fig, ax = plt.subplots()
-ax.plot(t, out_signal, label='out')
-ax.plot(t, orig_signal, label='original')
-ax.set_title('Using Convolution')
-ax.legend()
-plt.show()
 
-preprocessed_signal = np.r_[signal[window_len-1:0:-1], signal, signal[-2:-window_len-1:-1]]
-new_t = np.r_[t[window_len-1:0:-1], t, t[-2:-window_len-1:-1]]
-new_t = new_t[:, np.newaxis]
-knnregressor = KNeighborsRegressor(n_neighbors=window_len)
-knnregressor.fit(new_t, preprocessed_signal)
-ai_signal = knnregressor.predict(t[:, np.newaxis])
-fig, ax = plt.subplots()
-ax.plot(t, ai_signal, label='knnsignal')
-ax.plot(t, orig_signal, label='original')
-ax.set_title('Using KNeighboursRegressor')
-ax.legend()
-plt.show()
-
-#comparing results
-fig, ax = plt.subplots(nrows=2)
-ax[0].plot(t, out_signal, label='out')
-ax[0].plot(t, orig_signal, label='original')
-ax[0].set_title('Using Convolution')
-ax[0].legend()
-ax[1].plot(t, ai_signal, label='knnsignal')
-ax[1].plot(t, orig_signal, label='original')
-ax[1].set_title('Using KNeighboursRegressor')
-ax[1].legend()
-fig.tight_layout()
-plt.show()
+#using KNeighbours
+kregressor = KNeighborsRegressor(n_neighbors=15)
+kregressor.fit(t[:,np.newaxis], signal)
+ai_signal = kregressor.predict(t[:,np.newaxis])
 
 signal = pd.Series(signal)
 fig, ax = plt.subplots(nrows=2)
 ax[0].fill_between(t, signal - signal.rolling(window_len).std(), signal + signal.rolling(window_len).std(), alpha=0.5, facecolor='lightseagreen')
 ax[0].plot(t, out_signal, color='mediumseagreen')
-ax[0].set_title('Using Convolution')
+ax[0].set_xlabel('Time Index', fontsize=5)
+ax[0].set_ylabel('Amplitude', fontsize=5)
+ax[0].set_title('Using Convolution', fontsize=10)
+ax[0].set_xlim(t.min(), t.max())
 ax[1].fill_between(t, signal - signal.rolling(window_len).std(), signal + signal.rolling(window_len).std(), alpha=0.5, facecolor='lightseagreen')
 ax[1].plot(t, ai_signal, color = 'mediumseagreen')
-ax[1].set_title('Using KNeighboursRegressor')
+ax[1].set_xlabel('Time Index', fontsize=5)
+ax[1].set_ylabel('Amplitude', fontsize=5)
+ax[1].set_title('Using KNeighboursRegressor', fontsize=10)
+ax[1].set_xlim(t.min(), t.max())
 fig.tight_layout()
 plt.show()
 
@@ -96,9 +76,4 @@ plt.show()
 
 
 
-
-
-
-
-
-
+                                                                                                                                    
--- Convolution.py
+++ Convolution.py
@@ -5,13 +5,6 @@ class Convolution():
         
     def convolute(self, window, window_len, data):
 
-        '''fuction to convolute signal with the desired window type
-        with the desired length to smoothen it
-        window : window type in string
-        valid inputs : flat, hanning, hamming, blackman, bartlett
-        window_len : length of the window must be even if odd, will 
-        be rounded to nearest greater even integer'''
-
         if window not in ['flat', 'hanning', 'hamming', 'blackman', 'bartlett']:
             raise ValueError ("window must be one of the following: ['flat', 'hanning', 'hamming', 'blackman', 'bartlett']")
 
