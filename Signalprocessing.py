2f4beeee598263d58f05d05996f218a22df8598a
notsoharshdevisha <85506475+notsoharshdevisha@users.noreply.github.com>
2021-07-01 11:38:14 +0530

    Add files via upload

---
+++ Signalprocessing.py
@@ -0,0 +1,140 @@
+import numpy as np
+import cv2 as cv
+import warnings
+
+kernels = {'gblur5' : 1/233 * np.array([[1,  4,  7,  4, 1],
+                                        [4, 16, 26, 16, 4],
+                                        [7, 26, 41, 26, 7],
+                                        [4, 16, 26, 16, 4],
+                                        [1,  4,  7,  4, 1]]), 
+
+           'gblur3' : 1/16 * np.array([[1, 2, 1],
+                                       [2, 4, 2],
+                                       [1, 2, 1]]), 
+
+           'edge' : np.array([[-1, -1, -1],
+                              [-1,  8, -1],
+                              [-1, -1, -1]]),
+
+           'depth' : np.array([[-2, -1, 0],
+                               [-1,  1, 1],
+                               [ 0,  1, 2]]),
+
+           'sharpen' : np.array([[ 0, -1,  0],
+                                 [-1,  5, -1],
+                                 [ 0, -1,  0]]),
+
+           'denoise3' : 1/9 * np.ones((3,3)),
+
+           'denoise5' : 1/25 * np.ones((5,5))}
+
+
+class Convolution():
+        
+    def convolute(self, window, window_len, data):
+
+        if window not in ['flat', 'hanning', 'hamming', 'blackman', 'bartlett']:
+            raise ValueError ("window must be one of the following: ['flat', 'hanning', 'hamming', 'blackman', 'bartlett']")
+
+        if window_len % 2 != 0:
+            window_len +=1
+
+        #adding padding at begining and end of the signal to minimize data loss
+        preprocessed_signal = np.r_[data[window_len-1 : 0: -1], data, data[-2 : -window_len-1 : -1]]
+
+        if window == "flat":
+            w = np.r_[[0], np.ones(window_len-2), [0]]
+        else:
+            w = eval('np.'+window+'(window_len)')
+
+        #performing convolution
+        out_ = np.convolve(w/w.sum(), preprocessed_signal, mode='valid')
+       
+        #adjusting length of the convoluted signal
+        start = int(window_len/2)
+        end = -int(window_len/2 - 1)
+        out_signal = out_[start : end]
+
+        return out_signal
+
+    def convolute2d(self, img, kernel, pool_type, channels=None):
+
+        if kernel not in ['gblur3', 'gblur5', 'sharpen', 'edge', 'depth', 'denoise3', 'denoise5']:
+            raise ValueError("The kernel must be one of ['gblur3', 'gblur5', 'sharpen', 'edge', 'depth', 'denoise3', 'denoise5']")
+
+        if channels:
+            if channels != 1 and channels != 3:
+                raise ValueError('Number of channels must be either 1 or 3')
+
+        if kernel in  ['sharpen', 'edge', 'depth'] and pool_type != 'sum':
+            warnings.warn("Use pool_type='sum' while using any of ['sharpen', 'edge', 'depth'] for optimal reslts")
+
+        if channels == 1:
+            img = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
+
+        if len(img.shape) != 3:
+            img = img.reshape((img.shape[0], img.shape[1], 1))
+
+        channels = img.shape[2]
+
+        kernel_size = kernels[kernel].shape[0]
+        if kernel_size  == 3:
+            padded_img = np.zeros((img.shape[0]+2, img.shape[1]+2, channels))
+            padded_img[1:-1, 1:-1, 0:channels] = img
+        else:  
+            padded_img = np.zeros((img.shape[0]+4, img.shape[1]+4, channels))
+            padded_img[2:-2, 2:-2, 0:channels] = img
+
+        out_img = np.zeros(img.shape)
+        
+        for k in range(channels):
+            for i in range(img.shape[0]):
+                for j in range(img.shape[1]):
+                    out_img[i, j, k] = eval('np.'+pool_type+'(padded_img[i:i+kernel_size, j:j+kernel_size, k] * kernels[kernel])') 
+
+        return out_img
+
+    
+class fft():
+
+    def filter(self, img, type_, radius):
+
+        img = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
+        center = (img.shape[0]//2, img.shape[1]//2)
+
+        if center[0] % 2 == 0:
+            y = np.arange(-center[0], center[0]+1)
+        else:
+            y = np.arange(-center[0], center[0])
+
+        if center[1] % 2 == 0:
+            x = np.arange(-center[1], center[1]+1)
+        else:
+            x = np.arange(-center[1], center[1])
+
+        xx, yy = np.meshgrid(x, y)
+        
+        img_fft = np.fft.fft2(img)
+        img_fft_shifted = np.fft.fftshift(img_fft)
+
+        if type_ == 'lowpass':
+            lp_filt = np.zeros(img.shape)
+            lp_filt[xx**2 + yy**2 < radius**2] = 1
+            out_img = img_fft_shifted * lp_filt
+        elif type_ == 'highpass':
+            hp_filt = np.ones(img.shape)
+            hp_filt[xx**2 + yy**2 < radius**2] = 0
+            out_img = img_fft_shifted * hp_filt
+        else:
+            raise ValueError('type_ must be either lowpass or highpass')
+
+        out_img = np.fft.ifftshift(out_img) 
+        out_img = np.fft.ifft2(out_img)      
+
+
+        return np.real(out_img)
+        
+
+
+
+
---
+++ ppg_fft.py
@@ -0,0 +1,217 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import signal
+import collections
+import warnings
+warnings.filterwarnings('ignore')
+
+data = pd.read_csv('ppg_signal.csv')
+
+ppg = np.array(data['signal'])
+n = len(ppg)
+t = np.arange(n)
+fs = 64
+#visualizing the signal
+fig, ax = plt.subplots(figsize=(14, 8))
+ax.plot(t, ppg, color='lightseagreen', alpha=0.75, linewidth=0.75, label='Signal')
+ax.set_xlabel('Samples', fontsize=10)
+ax.set_ylabel('Signal', fontsize=10)
+ax.legend(prop={'size':9})
+ax.grid(linewidth=0.5, alpha=0.5)
+ax.set_title('PPG Signal', fontsize=15)
+ax.set_xlim(t.min(), t.max())
+plt.setp(ax.get_xticklabels(), fontsize=6)
+plt.setp(ax.get_yticklabels(), fontsize=6)
+plt.show()
+
+#using np.fft.fft
+ppg_fft = np.fft.fft(ppg)
+ppg_psd = ppg_fft * np.conjugate(ppg_fft) / n
+f = (fs/n) * np.arange(n)
+#visualizing PSD
+fig, ax = plt.subplots(figsize=(12, 8))
+ax.plot(f[:n//2], ppg_psd[:n//2], color = 'lightseagreen', alpha=0.75, label='fft*conj(fft)/n')
+ax.set_title("Power Spectral Density", fontsize=15)
+ax.set_xlabel("Frequency", fontsize=12)
+ax.set_ylabel("Power", fontsize=12)
+ax.grid(linewidth=0.5, alpha=0.5)
+ax.set_xlim(min(f), max(f[:n//2]))
+ax.legend(prop={'size':10})
+plt.setp(ax.get_xticklabels(), fontsize=7)
+plt.setp(ax.get_yticklabels(), fontsize=7)
+plt.show()
+
+#initializing the thresholds 
+thresh1 = 0.25*1e6           
+thresh2 = 0.5*1e6            
+
+#viz psd cutoff
+fig, ax = plt.subplots(figsize=(12, 8))
+ax.plot(f[:n//2], ppg_psd[:n//2], color = 'lightseagreen', alpha=0.75, label='fft*conj(fft)/n')
+ax.set_title("Power Spectral Density", fontsize=15)                                            
+ax.set_xlabel("Frequency", fontsize=12)                                                         
+ax.set_ylabel("Power", fontsize=12)                                                             
+ax.grid(linewidth=0.5, alpha=0.5)                                                              
+ax.set_xlim(min(f), max(f[:n//2]))                                                                                              
+ax.axhline(y=thresh1, c='black', label='thresh1', linestyle='--')
+ax.axhline(y=thresh2, c='black', label='thresh2', linestyle='--')
+plt.setp(ax.get_xticklabels(), fontsize=7)                                                     
+plt.setp(ax.get_yticklabels(), fontsize=7)                                                     
+ax.legend(prop={'size':10}) 
+plt.show()                                                                                     
+
+#Comparing filtered signal with different thresholds
+#'filtering'
+ppg_fft_thresh1 = [0 if ppg_psd[i]<thresh1 else ppg_fft[i] for i in range(n)]
+ppg_fft_thresh2 = [0 if ppg_psd[i]<thresh2 else ppg_fft[i] for i in range(n)]
+#computing ift of the 'filtered' signals
+ppg_thresh1 = np.fft.ifft(ppg_fft_thresh1)
+ppg_thresh2 = np.fft.ifft(ppg_fft_thresh2)
+#VIZ
+fig, ax = plt.subplots(nrows=2, figsize=(14, 8))
+ax[0].plot(t, ppg, color='lightseagreen', alpha=0.75, label='Original Signal', linewidth=0.75)
+ax[0].plot(t, ppg_thresh1, color='black', label='PSD > 0.25*10^6', linewidth=0.75)
+ax[0].set_title("Threshold : 0.25*10^6", fontsize=12)
+ax[0].set_xlabel('Samples', fontsize=10)
+ax[0].set_ylabel('Signal', fontsize=10)
+ax[0].legend(loc=1, prop={'size':9})
+ax[0].grid(linewidth=0.5, alpha=0.5) 
+ax[0].set_xlim(t.min(), t.max()) 
+ax[1].plot(t, ppg, color='lightseagreen', alpha=0.75, label='Original Signal', linewidth=0.75) 
+ax[1].plot(t, ppg_thresh2, color='black', label='PSD > 0.5*10^6', linewidth=0.75) 
+ax[1].set_title("Threshold : 0.5*10^6", fontsize=12)
+ax[1].set_xlabel('Samples', fontsize=10) 
+ax[1].set_ylabel('Signal', fontsize=10)  
+ax[1].legend(loc=1, prop={'size':9})    
+ax[1].grid(linewidth=0.5, alpha=0.5)    
+ax[1].set_xlim(t.min(), t.max())
+plt.setp(ax[0].get_xticklabels(), fontsize=5)
+plt.setp(ax[0].get_yticklabels(), fontsize=5) 
+plt.setp(ax[1].get_xticklabels(), fontsize=5)  
+plt.setp(ax[1].get_yticklabels(), fontsize=5)  
+fig.tight_layout()
+plt.show()
+ 
+# a more minute analysis
+fig, ax = plt.subplots(nrows=2, figsize=(14, 8))
+ax[0].plot(t[:fs*60], ppg[:fs*60], color='lightseagreen', label='Original Signal', linewidth=1.5)
+ax[0].plot(t[:fs*60], ppg_thresh1[:fs*60], color='black', label='PSD > 0.25*10^6', linewidth=0.75)
+ax[0].set_title("Threshold : 0.25*10^6", fontsize=12)
+ax[0].set_xlabel('Samples', fontsize=10)
+ax[0].set_ylabel('Signal', fontsize=10)
+ax[0].legend(loc=1, prop={'size':9})
+ax[0].grid(linewidth=0.5, alpha=0.5) 
+ax[0].set_xlim(t[:fs*60].min(), t[:fs*60].max()) 
+ax[1].plot(t[:fs*60], ppg[:fs*60], color='lightseagreen', label='Original Signal', linewidth=1.5) 
+ax[1].plot(t[:fs*60], ppg_thresh2[:fs*60], color='black', label='PSD > 0.5*10^6', linewidth=0.75) 
+ax[1].set_title("Threshold : 0.5*10^6", fontsize=12)
+ax[1].set_xlabel('Samples', fontsize=10) 
+ax[1].set_ylabel('Signal', fontsize=10)  
+ax[1].legend(loc=1, prop={'size':9})    
+ax[1].grid(linewidth=0.5, alpha=0.5)    
+ax[1].set_xlim(t[:fs*60].min(), t[:fs*60].max())
+plt.setp(ax[0].get_xticklabels(), fontsize=5)
+plt.setp(ax[0].get_yticklabels(), fontsize=5) 
+plt.setp(ax[1].get_xticklabels(), fontsize=5)  
+plt.setp(ax[1].get_yticklabels(), fontsize=5)  
+fig.tight_layout()
+plt.show()
+                        
+#using butterworth filter
+thresh1 = 1.5
+thresh2 = 1
+order = 10
+sos = signal.butter(order, thresh1, btype='lowpass', output='sos', fs=64)
+filtered_signal_1 = signal.sosfilt(sos, ppg)
+sos = signal.butter(order, thresh2, btype='lowpass', output='sos', fs=64)
+filtered_signal_2 = signal.sosfilt(sos, ppg)
+
+#visualizing the results
+fig, ax = plt.subplots(nrows=2, figsize=(14,8))
+ax[0].plot(t, ppg, color='lightseagreen', alpha=0.75, label='Original Signal')
+ax[0].plot(t, filtered_signal_1, color='black', label='lowpass : {}Hz'.format(str(thresh1)), linewidth=0.75)
+ax[0].set_xlabel('Samples', fontsize=10)
+ax[0].set_ylabel('Signal', fontsize=10)
+ax[0].grid(linewidth=0.5, alpha=0.5)
+ax[0].legend(loc=1, prop={'size':9})
+ax[0].set_xlim(t.min(), t.max())
+ax[0].set_title('Lowpass : {}Hz'.format(thresh1), fontsize=12)
+plt.setp(ax[0].get_xticklabels(), fontsize=7)
+plt.setp(ax[0].get_yticklabels(), fontsize=7)
+ax[1].plot(t, ppg, color='lightseagreen', alpha=0.75, label='Original Signal')                              
+ax[1].plot(t, filtered_signal_2, color='black', label='lowpass : {}Hz'.format(str(thresh2)), linewidth=0.75)
+ax[1].set_xlabel('Samples', fontsize=10)                                                                     
+ax[1].set_ylabel('Signal', fontsize=10)                                                                      
+ax[1].grid(linewidth=0.5, alpha=0.5)                                                                        
+ax[1].legend(loc=1, prop={'size':9})                                                                        
+ax[1].set_xlim(t.min(), t.max())                                                                            
+ax[1].set_title('Lowpass : {}Hz'.format(thresh2), fontsize=12)
+plt.setp(ax[1].get_xticklabels(), fontsize=7)                                                               
+plt.setp(ax[1].get_yticklabels(), fontsize=7)                                                               
+fig.suptitle('Using Butterworth Filter', fontsize=12)
+fig.tight_layout()
+plt.show()
+
+#for a minute comparison
+sec = 120
+fig, ax = plt.subplots(nrows=2, figsize=(14, 8))                                                                             
+ax[0].plot(t[:fs*sec], ppg[:fs*sec], color='lightseagreen', alpha=0.75, label='Original Signal')                              
+ax[0].plot(t[:fs*sec], filtered_signal_1[:fs*sec], color='black', label='lowpass : {}Hz'.format(str(thresh1)), linewidth=0.75)
+ax[0].set_xlabel('Samples', fontsize=10)                                                                     
+ax[0].set_ylabel('Signal', fontsize=10)                                                                      
+ax[0].grid(linewidth=0.5, alpha=0.5)                                                                        
+ax[0].legend(loc=1, prop={'size':9})                                                                        
+ax[0].set_xlim(t[:fs*sec].min(), t[:fs*sec].max())                                                                            
+ax[0].set_title('Lowpass : {}Hz'.format(thresh1), fontsize=12)                                               
+plt.setp(ax[0].get_xticklabels(), fontsize=7)                                                               
+plt.setp(ax[0].get_yticklabels(), fontsize=7)                                                               
+ax[1].plot(t[:fs*sec], ppg[:fs*sec], color='lightseagreen', alpha=0.75, label='Original Signal')                              
+ax[1].plot(t[:fs*sec], filtered_signal_2[:fs*sec], color='black', label='lowpass : {}Hz'.format(str(thresh2)), linewidth=0.75)
+ax[1].set_xlabel('Samples', fontsize=10)                                                                     
+ax[1].set_ylabel('Signal', fontsize=10)                                                                      
+ax[1].grid(linewidth=0.5, alpha=0.5)                                                                        
+ax[1].legend(loc=1, prop={'size':9})                                                                        
+ax[1].set_xlim(t[:fs*sec].min(), t[:fs*sec].max())                                                                            
+ax[1].set_title('Lowpass : {}Hz'.format(thresh2), fontsize=12)                                               
+plt.setp(ax[1].get_xticklabels(), fontsize=7)                                                               
+plt.setp(ax[1].get_yticklabels(), fontsize=7)                                                               
+fig.suptitle('Using Butterworth Filter', fontsize=12)                                                        
+fig.tight_layout()                                                                                          
+plt.show()      
+
+#comparing different order of the filter
+order1 = 1
+order2 = 10
+sos = signal.butter(order1, 1, btype='lowpass', output='sos', fs=64)
+filterd_signal_1 = signal.sosfilt(sos, ppg)
+sos = signal.butter(order2, 1, btype='lowpass', output='sos', fs=64)
+filtered_signal_2 = signal.sosfilt(sos, ppg)
+sec = 120                                                                                                                     
+fig, ax = plt.subplots(nrows=2, figsize=(14, 8))                                                                                               
+ax[0].plot(t[:fs*sec], ppg[:fs*sec], color='lightseagreen', alpha=0.75, label='Original Signal')                              
+ax[0].plot(t[:fs*sec], filtered_signal_1[:fs*sec], color='black', label='Filtered Signal', linewidth=0.75)
+ax[0].set_xlabel('Samples', fontsize=10)                                                                                       
+ax[0].set_ylabel('Signal', fontsize=10)                                                                                        
+ax[0].grid(linewidth=0.5, alpha=0.5)                                                                                          
+ax[0].legend(loc=1, prop={'size':9})                                                                                          
+ax[0].set_xlim(t[:fs*sec].min(), t[:fs*sec].max())                                                                            
+ax[0].set_title('Order : {}'.format(order1), fontsize=12)                                                                 
+plt.setp(ax[0].get_xticklabels(), fontsize=7)                                                                                 
+plt.setp(ax[0].get_yticklabels(), fontsize=7)                                                                                 
+ax[1].plot(t[:fs*sec], ppg[:fs*sec], color='lightseagreen', alpha=0.75, label='Original Signal')                              
+ax[1].plot(t[:fs*sec], filtered_signal_2[:fs*sec], color='black', label='Filtered Signal', linewidth=0.75)
+ax[1].set_xlabel('Samples', fontsize=10)                                                                                       
+ax[1].set_ylabel('Signal', fontsize=10)                                                                                        
+ax[1].grid(linewidth=0.5, alpha=0.5)                                                                                          
+ax[1].legend(loc=1, prop={'size':9})                                                                                          
+ax[1].set_xlim(t[:fs*sec].min(), t[:fs*sec].max())                                                                            
+ax[1].set_title('Order : {}'.format(order2), fontsize=12)                                                                 
+plt.setp(ax[1].get_xticklabels(), fontsize=7)                                                                                 
+plt.setp(ax[1].get_yticklabels(), fontsize=7)                                                                                 
+fig.suptitle('Using Butterworth Filter', fontsize=12)                                                                          
+fig.tight_layout()                                                                                                            
+plt.show()                                         
+
+
+
